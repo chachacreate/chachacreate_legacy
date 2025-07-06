a65f83447b9f058d0e536b7a6c06aaf10261ca6d
jahuiLee <jennysky46@gmail.com>
2025-07-06 20:28:32 +0900

    🐛 fix: 메인홈 상품 url 매핑 오류 수정

--- src/main/java/com/chacha/create/controller/controller/mainhome/main/HomeMainController.java
+++ src/main/java/com/chacha/create/controller/controller/mainhome/main/HomeMainController.java
@@ -1,7 +1,9 @@
 package com.chacha.create.controller.controller.mainhome.main;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.slf4j.Slf4j;
@@ -28,6 +30,12 @@ public class HomeMainController {
     public String showAllproductsPage() {
 		return "main/mainAllProducts";
     }
+    
+	// 상세페이지
+	@GetMapping("/productdetail/{productId}")
+	public String showProductdetail() {
+		return "store/productDetail";
+	}
     
     
     @GetMapping("/question")
