0518654ce8c7c9e3364310976d854c5d53917cdf
kirk0221 <99606072+kirk0221@users.noreply.github.com>
2025-07-30 15:57:19 +0900

    📦️ chore: db 기본 설정 스크립트 추가

--- files/db_user_create.sql
+++ files/db_user_create.sql
@@ -9,7 +9,7 @@ EXCEPTION
         END IF;
 END;
 /
--- Drop sequence audses$;
+
 Create sequence audses$ start with 1 increment by 1 minvalue 1 maxvalue 32000 cycle cache 20 noorder;
 CREATE USER chacha IDENTIFIED BY 1234;
 GRANT CONNECT, RESOURCE TO chacha;
