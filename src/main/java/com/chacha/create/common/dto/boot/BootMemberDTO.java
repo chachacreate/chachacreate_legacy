fd919aba59d9201bd533cdfe13b6fa0bbbe6942c
kirk0221 <99606072+kirk0221@users.noreply.github.com>
2025-09-04 12:18:10 +0900

    ✨ feat: Boot에서 Legacy 받아오는 Util처럼 BootAPIUtil 생성
    ✨ feat: Boot에서 Legacy 받아오는 Util처럼 BootAPIUtil 생성

--- src/main/java/com/chacha/create/common/dto/boot/BootMemberDTO.java
+++ src/main/java/com/chacha/create/common/dto/boot/BootMemberDTO.java
@@ -1,4 +1,4 @@
-package com.chacha.create.common.dto.store;
+package com.chacha.create.common.dto.boot;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
